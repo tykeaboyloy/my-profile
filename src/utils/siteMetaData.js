67edb15925bf089e7a768e5cec4e6640fdc84531
tk <tykeaboyloy@gmail.com>
2024-01-09 23:19:35 +0700

    update site meta data

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,12 +1,12 @@
 // please update required information
 const siteMetadata = {
   title: "TK",
-  author: "CodeBucks",
+  author: "Tykeaboyloy",
   headerTitle: "Next.js Blog",
   description: "A blog created with Next.js, Tailwind.css and contentlayer.",
   language: "en-us",
   theme: "system", // system, dark or light
-  siteUrl: "https://create-blog-with-nextjs.vercel.app", // your website URL
+  siteUrl: "https://tykea.appverze.com/", // your website URL
   siteLogo: "/logo.png",
   socialBanner: "/social-banner.png", // add social banner in the public folder
   email: "tykeaboyloy@gmail.com",
